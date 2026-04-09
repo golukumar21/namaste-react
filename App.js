7b20991a2ef89410f4ba2b52ba7fbf1783e5d930
Kumar abhishek <golukumar21@gmail.com>
2026-04-09 17:44:09 +0530

    episode-01

--- App.js
+++ App.js
@@ -1,4 +1,18 @@
-/** Creating Nested structure*/
+/**Creating React Element : START */
+
+// const heading = React.createElement(
+//   "h1", // element type
+//   { id: "heading", xyz: "abc" }, // props: attributes and values
+//   "Hello World from React!", // children: content inside the element
+// );
+// console.log(heading); // object
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading); // Coverts the React element to HTML and then adds it to the DOM
+
+/**Creating React Element : END */
+
+/** Creating Nested Structure: START*/
+
 /**
  * <div id="parent">
  *      <div id="child">
@@ -12,6 +26,7 @@
  * </div>
  * ReactElement(Object) => HTML(Browser Understands) => DOM(Browser Manipulates)
  */
+
 const parent = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child" }, [
     React.createElement("h1", {}, "I am an h1 tag"),
@@ -23,13 +38,8 @@ const parent = React.createElement("div", { id: "parent" }, [
   ]),
 ]);
 
-// const heading = React.createElement(
-//   "h1", // element type
-//   { id: "heading", xyz: "abc" }, // props: attributes and values
-//   "Hello World from React!", // children: content inside the element
-// );
-// console.log(heading); // object
-
 console.log(parent); // object
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(parent); // Coverts the React element to HTML and then adds it to the DOM
+
+/** Creating Nested Structure: END*/
